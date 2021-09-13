7b5cb231bef35bddf68b5e693bccd2e731727789
xafixav <xafiworld@gmail.com>
2021-09-13 16:03:33 -0300

    requirement 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const getAnimal = data.species.find((element) => element.name === animal);
+  const result = getAnimal.residents.every((element) => element.age >= age);
+  return result
 }
 
 module.exports = getAnimalsOlderThan;
