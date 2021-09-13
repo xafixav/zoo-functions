9a240577e943a091e63d433f09fd9e2342cc19ef
xafixav <xafiworld@gmail.com>
2021-09-13 15:29:27 -0300

    requirement 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,16 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-  console.log('let`s goooo')
+function getSpeciesByIds(id1, id2) {
+  let result = [];
+  if (!id1 && !id2) {
+    result = [];
+  } else if (!id2) {
+    result.push(data.species.find((element) => element.id === id1));
+  } else {
+    result.push(data.species.find((element) => element.id === id1));
+    result.push(data.species.find((element) => element.id === id2));
+  }
+  return result;
 }
-
+console.log(getSpeciesByIds());
 module.exports = getSpeciesByIds;
