1115e6f196a627d055de02654816e78179ef02f0
xafixav <xafiworld@gmail.com>
2021-09-13 16:03:53 -0300

    requirement 2 lint fix

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -3,7 +3,7 @@ const data = require('../data/zoo_data');
 function getAnimalsOlderThan(animal, age) {
   const getAnimal = data.species.find((element) => element.name === animal);
   const result = getAnimal.residents.every((element) => element.age >= age);
-  return result
+  return result;
 }
 
 module.exports = getAnimalsOlderThan;
