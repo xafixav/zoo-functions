1704a053b43b4a553c62c87c795c1d3c3b3e02ae
xafixav <xafiworld@gmail.com>
2021-09-14 17:22:21 -0300

    requirement 7 removed for rebuild

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,21 +1,4 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function buildMap() {  
-  const map = { NE: [], NW: [], SE: [], SW: [] };
-  map.NE = data.species.filter((spec) => spec.location === 'NE');
-  console.log(map)
-}
-
-function getAnimalMap(options) {
-//  let result = { NE: [], NW: [], SE: [], SW: [] };
-  const isSortTrue = options.some(() => options.sorted);
-  console.log(isSortTrue);
- // const isSexTrue = options.some(() => options.sex);
- // const isIncludeNamesTrue = options.some(() => options.includeNames);
- return 0;
-}
-
-// console.log(getAnimalMap({ includeNames: true, sorted: true })) */
-// console.log(buildMap())
 module.exports = getAnimalMap;
