2ba955db6d85ce94ca8f93e6fff73ca3dcf730d4
xafixav <xafiworld@gmail.com>
2021-09-14 17:20:10 -0300

    requirement 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  if (!entrants || entrants.length === undefined) {
+    return 0;
+  }
+  const childAge = entrants.filter((person) => person.age < 18).length;
+  const adultAge = entrants.filter((person) => person.age > 17 && person.age < 50).length;
+  const seniorAge = entrants.filter((person) => person.age > 49).length;
+  const result = { child: childAge, adult: adultAge, senior: seniorAge };
+  return result;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || entrants.length === undefined) {
+    return 0;
+  }
+  const entries = countEntrants(entrants);
+  const child = entries.child * data.prices.child;
+  const adult = entries.adult * data.prices.adult;
+  const senior = entries.senior * data.prices.senior;
+  return child + adult + senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,21 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+function buildMap() {  
+  const map = { NE: [], NW: [], SE: [], SW: [] };
+  map.NE = data.species.filter((spec) => spec.location === 'NE');
+  map.NE = map.NE.filter((spec) => spec.)
+  console.log(map)
 }
 
+function getAnimalMap(options) {
+  let result = { NE: [], NW: [], SE: [], SW: [] };
+  const isSortTrue = options.some(() => options.sorted);
+  console.log(isSortTrue);
+  const isSexTrue = options.some(() => options.sex);
+  const isIncludeNamesTrue = options.some(() => options.includeNames);
+}
+
+// console.log(getAnimalMap({ includeNames: true, sorted: true })) */
+console.log(buildMap())
 module.exports = getAnimalMap;
