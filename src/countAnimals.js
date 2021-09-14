2118c6031edab15be87b0e37c309cd7ed3bd4dd6
xafixav <xafiworld@gmail.com>
2021-09-14 15:26:30 -0300

    requirement 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
+const greatAnimal = () => {
+  const result = {};
+  data.species.forEach((ani) => {
+    result[`${ani.name}`] = ani.residents.length;
+  });
+  return result;
+};
+
 function countAnimals(animal) {
-  // seu código aqui
+  let allAnimals = {};
+  if (!animal) {
+    allAnimals = greatAnimal();
+  } else if (Object.keys(animal).length < 2) {
+    const spec = Object.values(animal)[0];
+    const thisAnimal = data.species.find((ani) => ani.name === spec);
+    allAnimals = thisAnimal.residents.length;
+  } else {
+    const spec = Object.values(animal)[0];
+    const thisAnimal = data.species.find((ani) => ani.name === spec);
+    const genderCount = thisAnimal.residents.filter((ani) => ani.sex === Object.values(animal)[1]);
+    allAnimals = genderCount.length;
+  }
+  return allAnimals;
 }
 
 module.exports = countAnimals;
