12d04a8c4abe9c24406fb3e094d48ff26577a44f
Xafixav <xafiworld@gmail.com>
2022-04-02 00:32:28 -0300

    :white_check_mark: 100%

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,52 @@
+/* eslint-disable editorconfig/editorconfig */
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getSpecies(id) {
+  const { name, location } = data.species.find((el) => el.id === id);
+  return [name, location];
+}
+
+function getByName(name) {
+  const { id: employeeId, firstName, lastName, responsibleFor } = data.employees
+  .find((el) => el.firstName.includes(name) || el.lastName.includes(name));
+  const allSpeciesNameAndLocation = responsibleFor.map((animalId) => getSpecies(animalId));
+  const result = {
+    id: employeeId,
+    fullName: `${firstName} ${lastName}`,
+    species: allSpeciesNameAndLocation.map((ani) => ani[0]),
+    locations: allSpeciesNameAndLocation.map((ani) => ani[1]),
+  };
+  return result;
+}
+
+function getById(id) {
+  const payload = data.employees
+  .find((el) => el.id === id);
+  if (!payload) {
+    throw new Error('Informações inválidas');
+  }
+  const { id: employeeId, firstName, lastName, responsibleFor } = payload;
+  const allSpeciesNameAndLocation = responsibleFor.map((animalId) => getSpecies(animalId));
+  const result = {
+    id: employeeId,
+    fullName: `${firstName} ${lastName}`,
+    species: allSpeciesNameAndLocation.map((ani) => ani[0]),
+    locations: allSpeciesNameAndLocation.map((ani) => ani[1]),
+  };
+  return result;
+}
+
+function getEmployeesCoverage(request) {
+  if (!request) { 
+    return data.employees.map((employer) => getByName(employer.firstName));
+  }
+  const { name, id } = request;
+  if (name) {
+    return getByName(name);
+  }
+  if (id) {
+    return getById(id);
+  }
 }
 
 module.exports = getEmployeesCoverage;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const [animalId] = data.employees.find((el) => el.id === id).responsibleFor;
+  const {residents} = data.species.find((el) => el.id === animalId);
+  const [{name, sex, age}] = residents.sort((a,b) => b.age - a.age);
+  return [name, sex, age];
 }
 
 module.exports = getOldestFromFirstSpecies;
